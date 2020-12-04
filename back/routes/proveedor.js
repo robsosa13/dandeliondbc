898990fcdfc6d44dc967ffae9293f3e5b28adc5b
robsosa13 <rob.sosa13@gmail.com>
2020-12-04 15:55:19 -0400

    test2

--- back/routes/proveedor.js
+++ back/routes/proveedor.js
@@ -5,9 +5,9 @@ var api = express.Router();
 
 
 api.post('/proveedor/registrar',proveedorController.registerProveedor);
-// api.put('/proveedor/editar/:id',proveedorController.editProveedor);
-// api.delete('/proveedor/eliminar/:id',proveedorController.deleteProveedor);
-// api.get('/proveedor/:id',proveedorController.get_postulante);
+//api.put('/proveedor/editar/:id',proveedorController.editProveedor);
+//api.delete('/proveedor/eliminar/:id',proveedorController.deleteProveedor);
+//api.get('/proveedor/:id',proveedorController.get_postulante);
 api.get('/proveedores',proveedorController.listProveedor);
 
 module.exports = api;
